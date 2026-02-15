6c5dfa0c6a1b876c579e852bf71d262f79bc5cf8
PwLDev <pgibran09@gmail.com>
2026-02-15 15:48:40 -0700

    feat(levels): init

---
+++ signbot/paginator.py
@@ -0,0 +1,75 @@
+import discord
+from discord.ui import View, Button
+from discord.ext import commands
+
+class Paginator(View):
+    """
+    a paginator i guess
+    """
+
+    def __init__(self, timeout: int = 300, show_counter: bool = True):
+        super().__init__(timeout)
+
+        self.current = 0
+        self.pages = None
+        self.total = 0
+
+        self.previous_button = Button(emoji=discord.PartialEmoji(name="\U000025c0"))
+        self.next_button = Button(emoji=discord.PartialEmoji(name="\U000025b6"))
+        self.ephemeral = False
+        self.ctx = None
+        self.message = None
+        self.counter = None
+        self.show_counter = show_counter
+
+    async def start(self, ctx: discord.Interaction | commands.Context, pages: list[discord.Embed], ephemeral: bool = False):
+        if isinstance(ctx, discord.Interaction):
+            ctx = await commands.Context.from_interaction(ctx)
+
+        self.pages = pages
+        self.total = len(pages)
+
+        self.previous_button.callback = self.previous
+        self.next_button.callback = self.next
+
+        self.add_item(self.previous_button)
+
+        if self.show_counter:
+            self.counter = Button(
+                label=f"{self.current + 1}/{self.total}",
+                style=discord.ButtonStyle.secondary,
+                disabled=True
+            )
+            self.add_item(self.counter)
+
+        self.add_item(self.next_button)
+
+        self.ephemeral = ephemeral
+        self.ctx = ctx
+        self.message = await ctx.send(embed=self.pages[self.current], view=self, ephemeral=self.ephemeral)
+
+    async def previous(self, interaction: discord.Interaction):
+        if interaction.user != self.ctx.author:
+            return await interaction.response.send_message(
+                content="You cannot control this paginator because you did not execute it.",
+                ephemeral=True
+            )
+
+        self.current = self.current - 1 % self.total
+        if self.counter:
+            self.counter.label = f"{self.current + 1}/{self.total}"
+
+        await self.message.edit(embed=self.pages[self.current], view=self)
+
+    async def next(self, interaction: discord.Interaction):
+        if interaction.user != self.ctx.author:
+            return await interaction.response.send_message(
+                content="You cannot control this paginator because you did not execute it.",
+                ephemeral=True
+            )
+
+        self.current = self.current + 1 % self.total
+        if self.counter:
+            self.counter.label = f"{self.current + 1}/{self.total}"
+
+        await self.message.edit(embed=self.pages[self.current], view=self)
